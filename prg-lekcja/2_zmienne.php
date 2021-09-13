d683146156c7af522bf88e47f15560f50f5eb781
Mikołaj Ptak <50557685+MikolajPtak@users.noreply.github.com>
2021-09-13 09:36:28 +0200

    Changing and adding
    13.09

--- prg-lekcja/2_zmienne.php
+++ prg-lekcja/2_zmienne.php
@@ -26,4 +26,22 @@ else {
     echo "różne"
 }
 echo "<br>"
+
+$x=2
+echo ++$x; //3 preinkrementacja
+echo $x++; //3 postinkrementacja
+echo ++$x; //5 ++$x najpierw modyfikuje, potem wyświetla
+
+$x=1;
+echo $x;//1
+$x=$x++;//przypisje x=1 a potem zwiększa x o 1, ale kiedy zwiększasz tą samą zmienną to pomijamy postinkrementację
+echo $x;//1
+$x=++$x;// dodaje 
+echo $x;//2
+$y=++$x;
+echo $x;//3
+echo $y;//3
+$y=$x++;
+echo $x;//4
+echo "$y<br>";//3 
 ?>
