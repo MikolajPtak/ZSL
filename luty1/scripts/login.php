13d42af9e49c09e4b1675538da735929e091a245
Mikołaj Ptak <50557685+MikolajPtak@users.noreply.github.com>
2022-02-21 10:28:51 +0100

    changes

--- luty1/scripts/login.php
+++ luty1/scripts/login.php
@@ -1,6 +1,11 @@
 <?php
 require_once 'connect.php';
-$sql="SELECT pass FROM users WHERE login=$_POST[]";
+$sql='SELECT pass FROM users WHERE login=`$_POST[login]`';
 $result=$connect->query($sql);
+if (!empty($connect->affected_rows)) {
+  echo "dziala";
+}
+echo $result;
+echo "dziala?";
 
  ?>
